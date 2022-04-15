49df1324eba85aac19d47c824a43c43ed07fc589
GMartinez0210 <genaro010204@gmail.com>
2022-04-14 19:28:31 -0500

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function App() {
+  return (
+    <main className="container main-container">
+      <section className="row">
+        <div className="col col-6 mx-auto score-contianer">
+          <blockquote>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </blockquote>
+          <blockquote>
+            <p>SCORE</p>
+            <p>12</p>
+          </blockquote>
+        </div>
+      </section>
+      <section>
+        
+      </section>
+    </main>
+  );
+}
+
+export default App;
